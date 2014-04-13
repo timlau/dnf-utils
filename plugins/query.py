fd14a74f3c45d2422f96e7a3582e4b6dff267311
Tim Lauridsen <timlau@fedoraproject.org>
2014-04-13 17:59:42 +0200

    query: added --queryformat option to specify an rpm QUERYFMT like output format

--- plugins/query.py
+++ plugins/query.py
@@ -14,12 +14,17 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 from dnfutils import logger, _, ArgumentParser
 
 import dnf
 import dnf.cli
 import dnf.exceptions
 import hawkey
+import re
+
+# march a %[-][dd]{attr}
+QF_MATCH = re.compile('%([-\d]*?){([:\.\w]*?)}')
 
 
 class Query(dnf.Plugin):
@@ -38,15 +43,39 @@ class QueryCommand(dnf.cli.Command):
     """ the util command there is extending the dnf command line """
     aliases = ['query']
     activate_sack = True
-
     # summary for util, shown in dnf help
     summary = _('search for packages matching keyword')
     # usage string for the util
     usage = _('[OPTIONS] [KEYWORDS]')
 
-    def show_packages(self, query):
+    def get_format(self, qf):
+        ''' convert a rpm like QUERYFMT to an python .format() string'''
+        def fmt_repl(matchobj):
+            fill = matchobj.groups()[0]
+            key = matchobj.groups()[1]
+            if fill:
+                if fill[0] == '-':
+                    fill = '>' + fill[1:]
+                else:
+                    fill = '<' + fill
+                fill = ':' + fill
+            return '{0.' + key.lower() + fill + "}"
+
+        if not qf:
+            qf = '%{name}-%{epoch}:%{version}-%{release}.%{arch}'
+        qf = qf.replace("\\n", "\n")
+        qf = qf.replace("\\t", "\t")
+        fmt = re.sub(QF_MATCH, fmt_repl, qf)
+        return fmt
+
+    def show_packages(self, query, fmt):
         for pkg in query.run():
-            print(pkg)
+            try:
+                print(fmt.format(pkg))
+            except AttributeError as e:
+                # catch that the user has specified attributes
+                # there don't exist on the dnf Package object.
+                raise dnf.exceptions.Error(str(e))
 
     def run(self, args):
         ''' execute the util action here '''
@@ -60,6 +89,8 @@ class QueryCommand(dnf.cli.Command):
                             help='query in all packages')
         self.parser.add_argument("--latest", action='store_true',
                             help='show only latest packages')
+        self.parser.add_argument("--queryformat",
+                            help='format for displaying found packages')
         logger.debug('Command sample : run')
         try:
             opts = self.parser.parse_args(args)
@@ -76,7 +107,7 @@ class QueryCommand(dnf.cli.Command):
         q = q.filter(hawkey.ICASE, **fdict)
         if opts.latest:
             q.latest()
-        self.show_packages(q)
-
-
+        fmt = self.get_format(opts.queryformat)
+        print(fmt)
+        self.show_packages(q, fmt)
         return 0, ''
---
+++ tests/test_query.py
@@ -0,0 +1,63 @@
+#    dnf-utils - add-on tools for DNF
+#    Copyright (C) 2014 Tim Lauridsen < timlau<AT>fedoraproject<DOT>org >
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from support import mock
+
+import unittest
+import query
+
+class Pkg:
+    def __init__(self):
+        self.name = "foobar"
+        self.version = '1.0.1'
+        self.release = '1.f20'
+        self.arch = 'x86_64'
+        self.reponame = "@System"
+
+class SampleCommandTest(unittest.TestCase):
+
+    def test_get_format(self):
+        cli = mock.Mock()
+        cmd = query.QueryCommand(cli)
+        fmt = cmd.get_format('%{name}')
+        self.assertEqual(fmt, '{0.name}')
+        fmt = cmd.get_format('%40{name}')
+        self.assertEqual(fmt, '{0.name:<40}')
+        fmt = cmd.get_format('%-40{name}')
+        self.assertEqual(fmt, '{0.name:>40}')
+        fmt = cmd.get_format('%{name}-%{repoid} :: %-40{arch}')
+        self.assertEqual(fmt, '{0.name}-{0.repoid} :: {0.arch:>40}')
+
+    def test_output(self):
+        cli = mock.Mock()
+        cmd = query.QueryCommand(cli)
+        pkg = Pkg()
+        fmt = cmd.get_format('%{name}')
+        self.assertEqual(fmt.format(pkg), 'foobar')
+        fmt = cmd.get_format(
+            '%{name}-%{version}-%{release}.%{arch} (%{reponame})')
+        self.assertEqual(fmt.format(pkg),
+            'foobar-1.0.1-1.f20.x86_64 (@System)')
+
+    def test_illegal_attr(self):
+        cli = mock.Mock()
+        cmd = query.QueryCommand(cli)
+        pkg = Pkg()
+        with self.assertRaises(AttributeError) as e:
+            cmd.get_format('%{notfound}').format(pkg)
+            self.assertEqual(str(e),
+                "Pkg instance has no attribute 'notfound'")
